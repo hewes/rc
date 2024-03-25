10c67b5ea6e54fa51c91b266b1b42a471be3d0da
hewes <hrysk1986@gmail.com>
2024-03-25 10:31:03 +0900

    remove K keymap from normal mode

--- nvim/plugin.d/mason.lua
+++ nvim/plugin.d/mason.lua
@@ -16,7 +16,6 @@ local on_attach = function(client, bufnr)
   local set = vim.keymap.set
   --set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
   --set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-  set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
   set("n", "<C-m>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
   --set("n", "gy", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
   set("n", "rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
